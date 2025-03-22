8ca626d1466d6a8e3de5e9f86eaa1b3fd7c97794
woozway <22060468@zju.edu.cn>
2025-03-23 00:12:45 +0800

    0x14

--- 0x10 基本数据结构/0x14 Hash/AcWing138.py
+++ 0x10 基本数据结构/0x14 Hash/AcWing138.py
@@ -1,17 +1,24 @@
 N = 1000010
+M = 10**9 + 7
 s, f, p = [0]*N, [0]*N, [0]*N
+
+def rd():
+    lrs = []
+    while len(lrs) < 4:
+        lrs.extend(map(int, input().split()))
+    return lrs
 
 def main():
     s[1:] = list(input())
     n = len(s[1:])
     p[0] = 1
     for i in range(1, n+1):
-        f[i] = f[i-1]*131 + (ord(s[i])-ord('a')+1)
-        p[i] = p[i-1]*131
+        f[i] = (f[i-1]*131 + (ord(s[i])-ord('a')+1)) % M
+        p[i] = p[i-1]*131 % M
     m = int(input())
     for _ in range(m):
-        l1, r1, l2, r2 = [int(x) for x in input().split()]
-        if f[r1] - f[l1-1]*p[r1-l1+1] == f[r2] - f[l2-1]*p[r2-l2+1]:
+        l1, r1, l2, r2 = rd()
+        if (f[r1] - f[l1-1]*p[r1-l1+1])%M == (f[r2] - f[l2-1]*p[r2-l2+1])%M:
             print("Yes")
         else:
             print("No")
--- 0x10 基本数据结构/0x14 Hash/AcWing139.py
+++ 0x10 基本数据结构/0x14 Hash/AcWing139.py
@@ -1,24 +1,25 @@
 N, P = 1000006, 131
+M = 10**9 + 7 # MOD
 f1, f2, p = [0]*N, [0]*N, [0]*N
 
 def H1(i, j):
-    return f1[j] - f1[i-1]*p[j-i+1]
+    return (f1[j] - f1[i-1]*p[j-i+1]) % M
     
 def H2(i, j):
-    return f2[i] - f2[j+1]*p[j-i+1]
+    return (f2[i] - f2[j+1]*p[j-i+1]) % M
     
 def main():
     id_ = 0
     p[0] = 1
-    for i in range(1, N): p[i] = (p[i-1] * P) & 0xffffffffffffffff
+    for i in range(1, N): p[i] = (p[i-1] * P) % M
     while True:
         s = input()
         if s == 'END': break
         s = [0] + list(s)
         ans, len_ = 0, len(s[1:])
         f1[0], f2[len_+1] = 0, 0
-        for i in range(1, len_+1): f1[i] = f1[i-1]*P + ord(s[i])
-        for i in range(len_, 0, -1): f2[i] = f2[i+1]*P + ord(s[i])
+        for i in range(1, len_+1): f1[i] = (f1[i-1]*P + ord(s[i])) % M
+        for i in range(len_, 0, -1): f2[i] = (f2[i+1]*P + ord(s[i])) % M
         for i in range(1, len_+1):
             l, r = 0, min(i-1, len_-i)
             while l < r:
