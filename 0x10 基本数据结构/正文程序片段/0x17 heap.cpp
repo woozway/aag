65b8d769d71e176da6f240cf5cb8fb06bab33368
woozway <22060468@zju.edu.cn>
2025-03-30 11:01:12 +0800

    0x17

--- 0x10 基本数据结构/正文程序片段/0x17 heap.cpp
+++ 0x10 基本数据结构/正文程序片段/0x17 heap.cpp
@@ -1,27 +1,27 @@
-// 二叉堆
+// 二叉堆，下标从1开始
 int heap[SIZE], n;
-void up(int p) {
+void up(int p) { // 向上调整
   while (p > 1) {
-    if (heap[p] > heap[p/2]) {
+    if (heap[p] > heap[p/2]) { // 子节点>父节点，不满足大根堆性质
       swap(heap[p], heap[p/2]);
-      p/=2;
+      p /= 2;
     }
     else break;
   }	
 }
 
 void down(int p) {
-  int s = p*2;
+  int s = p * 2; // p的左子节点
   while (s <= n) {
-    if (s < n && heap[s] < heap[s+1]) s++;
-    if (heap[s] > heap[p]) {
+    if (s<n && heap[s]<heap[s+1]) s++; // 左右子节点取较大者
+    if (heap[s] > heap[p]) { // 子节点大于腹肌诶单，不满足大根堆性质
       swap(heap[s], heap[p]);
-      p = s, s = p*2;
+      p = s, s = p * 2;
     }
     else break;
   }
 }
-
+// 新节点放在二叉堆数组末尾，通过交换向上调整
 void Insert(int val) {
   heap[++n] = val;
   up(n);
@@ -30,12 +30,12 @@ void Insert(int val) {
 int GetTop() {
   return heap[1];
 }
-
+// 把堆顶heap[1]与末尾节点交换，然后移除末尾节点；向下调整
 void Extract() {
   heap[1] = heap[n--];
   down(1);
 }
-
+// 删除数组下标k位置的节点时，交换heap[k]和heap[n]后可能需要向上/下调整，分别检查
 void Remove(int k) {
   heap[k] = heap[n--];
   up(k), down(k);
