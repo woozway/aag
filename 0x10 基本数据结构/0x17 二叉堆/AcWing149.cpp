59716daf987e6acab966668896a01457f28d8b8a
woozway <22060468@zju.edu.cn>
2025-04-02 21:51:05 +0800

    0x17

--- 0x10 基本数据结构/0x17 二叉堆/AcWing149.cpp
+++ 0x10 基本数据结构/0x17 二叉堆/AcWing149.cpp
@@ -14,13 +14,13 @@ int main() {
   }
   while ((n-1) % (k-1)) {
     ++n; // k叉Huffman树，让子节点不足k个的情况发生在最底层
-    q.push(make_pair(0, 0)); // (-单词出现频率，-最长k进制串s的最短长度)
+    q.push(make_pair(0, 0)); // (-单词出现频率，-k进制串si的最大长度)
   }
   ll ans = 0;
   while (q.size() != 1) {
     ll num = 0, w = 0;
     for (int i=1; i<=k; i++) {
-      num += q.top().first;
+      num += q.top().first; // 取出的k个节点，不仅要a最小，w也要最小
       w = min(w, q.top().second); // 取出当前的k个节点中k进制串s长度最大的
       q.pop();
     }
