6c672554441e3e87aa343d60bbf96636fa36d98d
woozway <22060468@zju.edu.cn>
2025-04-09 23:51:49 +0800

    ch02

--- 0x10 基本数据结构/习题/AcWing159.cpp
+++ 0x10 基本数据结构/习题/AcWing159.cpp
@@ -4,11 +4,11 @@ using namespace std;
 const int N = 10010, M = 80;
 int n, m, nxt[N];
 char s[N][M];
-bool st[M]; // st[i]=1说明长度为i的循环节可行
-
+bool st[M];
+// 因为覆盖时最后一个可以多余，所以并不是简单求每个字符串的最小循环节
 int main() {
   cin >> n >> m;
-  memset(st, 1, sizeof st);
+  memset(st, 1, sizeof st); // st[i]==1说明长度为i的循环节可以覆盖
   for (int i=1; i<=n; i++) { // 判断每个字符串中宽度j的循环节是否可行
     scanf("%s", s[i]);
     for (int j=1; j<=m; j++)
@@ -23,7 +23,7 @@ int main() {
         }
   }
   int width;
-  for (int i=1; i<=m; i++)
+  for (int i=1; i<=m; i++) // 找到最小的循环节长度
     if (st[i]) {
       width = i;
       break;
