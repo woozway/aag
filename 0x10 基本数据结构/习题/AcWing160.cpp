3c938291c3162b240cadb8548892ede1bb648c67
woozway <22060468@zju.edu.cn>
2025-04-10 23:00:23 +0800

    ch02

--- 0x10 基本数据结构/习题/AcWing160.cpp
+++ 0x10 基本数据结构/习题/AcWing160.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 const int N = 200010;
-int f[N], nxt[N], cnt[N];
+int nxt[N], cnt[N];
 char a[N], b[N];
 int n, m, q;
 // 转化为有向无环图DAG
@@ -11,19 +11,19 @@ int main() {
   scanf("%s", b+1); // B[1~n]保存B串
   // 对B串自匹配，求next数组
   for (int i=2, j=0; i<=m; i++) {
-    while (j>0 && b[j+1]!=b[i]) j = nxt[j];
-    if (b[j+1] == b[i]) j++;
+    while (j>0 && b[i]!=b[j+1]) j = nxt[j];
+    if (b[i] == b[j+1]) j++;
     nxt[i] = j;
   }
-  // f[i]表示“A中以i结尾的子串”与“B的前缀”能够匹配的最大长度
+  // cnt[j]：A中以i为结尾的子串中和B匹配的最大长度为j
   for (int i=1, j=0; i<=n; i++) { // A串与B串进行模式匹配
-    while (j>0 && (j==m || a[i]!=b[j+1])) j = nxt[j];
+    while (j>0 && a[i]!=b[j+1]) j = nxt[j];
     if (a[i] == b[j+1]) j++;
-    f[i] = j;
+    cnt[j]++;
   }
+  for (int i=m; i; i--) cnt[nxt[i]] += cnt[i];
+  // cnt[x]++了多少次，cnt[next[x]]++也就要相应执行多少次
   // 计算完毕后cnt[x]保存的是匹配长度>=x的位置个数
-  for (int i=1; i<=n; i++) cnt[f[i]]++;
-  for (int i=n; i; i--) cnt[nxt[i]] += cnt[i];
   while (q--) {
     int x;
     scanf("%d", &x);
