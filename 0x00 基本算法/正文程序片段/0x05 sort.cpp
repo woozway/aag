bdd141dd4a59391c3d20c028ba21bee6ad914f03
woozway <wuzw@zju.edu.cn>
2025-04-15 23:58:49 +0800

    -
    -

--- 0x00 基本算法/正文程序片段/0x05 sort.cpp
+++ 0x00 基本算法/正文程序片段/0x05 sort.cpp
@@ -18,7 +18,7 @@ void merge(int l, int mid, int r) {
   // a是待排序数组, b是临时数组, cnt是逆序对个数
   int i = l, j = mid + 1;
   for (int k = l; k <= r; k++)
-    if (j > r || i <= mid && a[i] < a[j]) b[k] = a[i++];
+    if (j > r || i <= mid && a[i] <= a[j]) b[k] = a[i++];
     else b[k] = a[j++], cnt += mid - i + 1;
   for (int k = l; k <= r; k++) a[k] = b[k];
 }
